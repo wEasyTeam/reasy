6b2c5c9912e6c8003e7ab35e7d98ce97b22fec46
lwdgit <1127622940@qq.com>
2015-07-24 10:42:34 +0800

    first commit

---
+++ bin/reasy.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var Liftoff = require('liftoff');
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var cli = new Liftoff({
+  name: 'reasy', // 命令名字
+  processTitle: 'reasy',
+  moduleName: 'reasy',
+  configName: 'fis-conf',
+
+  // only js supported!
+  extensions: {
+    '.js': null
+  }
+});
+
+cli.launch({
+  cwd: argv.r || argv.root,
+  configPath: argv.f || argv.file
+}, function(env) {
+  var fis;
+  if (!env.modulePath) {
+    fis = require('../');
+  } else {
+    fis = require(env.modulePath);
+  }
+  fis.set('system.localNPMFolder', path.join(env.cwd, 'node_modules/reasy'));
+  fis.set('system.globalNPMFolder', path.dirname(__dirname));
+  fis.cli.run(argv, env);
+});
---
+++ index.js
@@ -0,0 +1,98 @@
+var fis = module.exports = require('fis3');
+
+fis.require.prefixes.unshift('reasy');
+fis.cli.name = 'reasy';
+fis.cli.info = require('./package.json');
+
+
+//使用相对路径
+fis.hook('relative');
+fis.match('**', {
+    relative: true
+});
+
+// 所有js, css 加 hash
+fis.match('*.{js,css,less,scss}', {
+    useHash: true
+});
+
+// 所有图片加 hash
+fis.match('image', {
+    useHash: true
+});
+
+// fis-parser-less
+fis.match('*.less', {
+    parser: fis.plugin('less'),
+    rExt: '.css'
+});
+
+fis.match('**/*.scss', {
+    rExt: '.css', // from .scss to .css
+    parser: fis.plugin('sass2', {
+        outputStyle: 'expanded',
+        sourceMap: false
+    })
+});
+
+fis.match('**/_*.scss', {
+    release: false
+});
+
+fis.match('**/*.{tpl,asp,html,htm}', {
+    parser: fis.plugin('art-template', {
+        native: false,
+        define: {
+            product: 'TENDA'
+        }
+    })
+});
+
+fis.match('**/*.tpl', {
+    release: false
+});
+
+
+fis.match('::package', {
+    spriter: fis.plugin('csssprites')
+});
+
+
+//fis3-hook-module
+fis.hook('module', {
+    mode: 'cmd',
+
+    // 记得设置这个。
+    forwardDeclaration: true,
+
+    baseUrl: './modules/',
+    paths: {
+
+    }
+});
+
+fis.match('modules/**.js', {
+    isMod: true
+});
+
+fis.match('./modules/sea.js', {
+  isMod: false
+}, true);
+
+fis.match('::packager', {
+  postpackager: fis.plugin('loader')
+});
+
+
+fis.media('product')
+.match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+})
+
+.match('*.{css,less}', {
+    optimizer: fis.plugin('clean-css')
+})
+
+.match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+});
