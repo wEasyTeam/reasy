4cfeb92ae34590bc194238843aa3460c2b46dae1
lwdgit <1127622940@qq.com>
2015-08-18 17:45:34 +0800

    modify logo

--- libs/logo.js
+++ libs/logo.js
@@ -1,36 +1,38 @@
 module.exports = function(reasy) {
     /*
-     \\\\\\\   \\\\\\\     \\      \\\\\\  \\    \\ 
-     \\    \\  \\         \\\\    \\   \\   \\  \\  
-     \\    \\  \\         \  \    \\\        \\ \   
-     \\\\\\\   \\\\\\\   \\  \\     \\\\      \\    
-     \\  \\    \\        \\\\\\        \\\    \\    
-     \\   \\   \\       \\    \\  \\    \\    \\    
-     \\    \\  \\\\\\\  \\     \   \\\\\\     \\    
+       //////    ///////    //      /////   //    ///
+      //    //  //         ///    //   //   //  ///
+     //    //  //        // //    //        // //
+    ///////   ///////   //  //     ////     ///
+   //   /    //       ///////  /     //     //
+  //    /   //       //    //  //  ///     //
+ //     // //////// //     //   ///       // 
     */
     reasy.cli.version = function() {
 
         var logo = '';
 
         if (reasy.util.isWin()) {
-            logo = '\n\n'
-            + '  \\\\\\\\\\\\\\'.bold.red + '   \\\\\\\\\\\\\\'.bold.yellow + '     \\\\'.bold.blue + '      \\\\\\\\\\\\'.bold.green + '  \\\\    \\\\'.bold.white + ' \n'
-            + '  \\\\    \\\\'.bold.red + '  \\\\'.bold.yellow + '         \\\\\\\\'.bold.blue + '    \\\\   \\\\'.bold.green + '   \\\\  \\\\'.bold.white + '  \n'
-            + '  \\\\    \\\\'.bold.red + '  \\\\'.bold.yellow + '         \\  \\'.bold.blue + '    \\\\\\'.bold.green + '        \\\\ \\'.bold.white + '   \n'
-            + '  \\\\\\\\\\\\\\'.bold.red + '   \\\\\\\\\\\\\\'.bold.yellow + '   \\\\  \\\\'.bold.blue + '     \\\\\\\\'.bold.green + '      \\\\'.bold.white + '    \n'
-            + '  \\\\  \\\\'.bold.red + '    \\\\'.bold.yellow + '        \\\\\\\\\\\\'.bold.blue + '        \\\\\\'.bold.green + '    \\\\'.bold.white + '    \n'
-            + '  \\\\   \\\\'.bold.red + '   \\\\'.bold.yellow + '       \\\\    \\\\'.bold.blue + '  \\\\    \\\\'.bold.green + '    \\\\'.bold.white + '    \n'
-            + '  \\\\    \\\\'.bold.red + '  \\\\\\\\\\\\\\'.bold.yellow + '  \\\\     \\'.bold.blue + '   \\\\\\\\\\\\'.bold.green + '     \\\\'.bold.white + '     \n';
+
+           logo = '\n\n' +
+                  '        //////    '.bold.red + '///////'.bold.yellow + '    //     '.bold.blue + ' /////  '.bold.green + ' //    ///\n'.bold.white +
+                  '       //    //  '.bold.red + '//     '.bold.yellow + '    ///    '.bold.blue + '//   // '.bold.green + '  //  /// \n'.bold.white +
+                  '      //    //  '.bold.red + '//     '.bold.yellow + '   // //   '.bold.blue + ' //     '.bold.green + '   // //   \n'.bold.white +
+                  '     ///////   '.bold.red + '///////'.bold.yellow + '   //  //  '.bold.blue + '   //// '.bold.green + '    ///     \n'.bold.white +
+                  '    //   /    '.bold.red + '//     '.bold.yellow + '  ///////  '.bold.blue + '/     //'.bold.green + '     //      \n'.bold.white +
+                  '   //    /   '.bold.red + '//     '.bold.yellow + '  //    // '.bold.blue + ' //  ///'.bold.green + '     //       \n'.bold.white +
+                  '  //     // '.bold.red + '////////'.bold.yellow + ' //     //'.bold.blue + '   ///  '.bold.green + '     //        \n'.bold.white;
+
         } else {
 
-            logo = '\n\n'
-            + '  \\\\\\\\\\\\\\'.bold.red + '   \\\\\\\\\\\\\\     \\\\      \\\\\\\\\\\\  \\\\    \\\\ \n'
-            + '  \\\\    \\\\  \\\\         \\\\\\\\    \\\\   \\\\   \\\\  \\\\  \n'
-            + '  \\\\    \\\\  \\\\         \\  \\    \\\\\\        \\\\ \\   \n'
-            + '  \\\\\\\\\\\\\\   \\\\\\\\\\\\\\   \\\\  \\\\     \\\\\\\\      \\\\    \n'
-            + '  \\\\  \\\\    \\\\        \\\\\\\\\\\\        \\\\\\    \\\\    \n'
-            + '  \\\\   \\\\   \\\\       \\\\    \\\\  \\\\    \\\\    \\\\    \n'
-            + '  \\\\    \\\\  \\\\\\\\\\\\\\  \\\\     \\   \\\\\\\\\\\\     \\\\     \n';
+            logo = '\n\n' +
+                   '       //////    ///////    //      /////   //    /// \n' +          
+                   '      //    //  //         ///    //   //   //  ///   \n' +        
+                   '     //    //  //        // //    //        // //     \n' +      
+                   '    ///////   ///////   //  //     ////     ///       \n' +    
+                   '   //   /    //       ///////  /     //     //        \n' +   
+                   '  //    /   //       //    //  //  ///     //         \n' +  
+                   ' //     // //////// //     //   ///       //          \n'; 
         }
         console.log(logo + '\n  Reasy v' + reasy.cli.info.version);
     };
