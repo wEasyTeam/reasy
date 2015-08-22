7a7a38a90d3c1c74564fe911875c05291705a5dc
lwdos <1127622940@qq.com>
2015-08-23 00:13:37 +0800

    install.bat support utf8

--- reasy-install.bat
+++ reasy-install.bat
@@ -1,4 +1,9 @@
+REM 请将下面这段代码保存到bat格式的文件中执行
+REM 注意：复制时请不要删除中文后面的全角空格
+
 @echo off
+@chcp 65001
+::用于解决uft8在控制台输出中文乱码的问题
 goto :START
 exit
 
@@ -11,18 +16,18 @@ exit /b 0
 :CHECK
 call :IF_EXIST node.exe
 if %errorlevel%==1 (
-    echo ���ϵͳû�а�װnode,�޷����б���
+    echo 你的系统没有安装node,无法进行编译　　　　　　　　　　　　　　　　　　　　　　　　　 
     goto :EXIST
 )
 
 call :IF_EXIST reasy.cmd
 if %errorlevel%==1 (
-    echo ���ϵͳû�а�װreasy���빤�ߣ������Զ���װ ,������Ҫ������
+    echo 你的系统没有安装reasy编译工具，正在自动安装 ,可能需要数分钟　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 
     npm install -g reasy --registry=https://registry.npm.taobao.org
     npm install -g web-debug --registry=https://registry.npm.taobao.org
-    call :IF_EXIST reasy.cmd || echo reasy���빤�߰�װʧ��
+    call :IF_EXIST reasy.cmd || echo reasy编译工具安装失败　　　　　　　　　　　　　　　　　　　　　　　　　 
 ) else (
-	echo ���ϵͳ�Ѿ���װreasy���빤�ߣ��汾Ϊ��
+	echo 你的系统已经安装reasy编译工具，版本为：　　　　　　　　　　　　　　　　　　　　　　　　　 
 )
 goto :VERSION
 
