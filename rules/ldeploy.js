2e52fa7cfb13c652f1c4a1d356553b709e76517b
lwdos <1127622940@qq.com>
2015-09-05 20:17:03 +0800

    add ldeploy

---
+++ rules/ldeploy.js
@@ -0,0 +1,39 @@
+function replacer(opt, fis) {
+    if (!opt)return;
+    if (!Array.isArray(opt)) {
+        opt = [opt];
+    }
+    var r = [];
+    opt.forEach(function(raw) {
+        if (!raw.to)return;
+        if (!raw.from) {//如果没有定义from,则使用local-deliver
+            r.push(fis.plugin('local-deliver', raw));
+        } else {
+            r.push(fis.plugin('replace', raw));
+        }  
+    });
+    return r;
+}
+
+/*
+ args example
+ [
+     {
+        to: '/home/xxx/webs'
+     },
+     {
+         from: 'a',
+         to: 'b',
+     },
+     {
+         from: 'a0',
+         to: 'b0'
+     }
+ ]*/
+
+
+module.exports = function(args) {
+    return this.match('**', {
+        deploy: replacer(args, this)
+    });
+};
