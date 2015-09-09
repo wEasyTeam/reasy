4e90fbcfca0452526e84b103fec9aa30da8dc32a
lwdgit <1127622940@qq.com>
2015-09-09 17:10:00 +0800

    fix bug

--- libs/middleHandleCli.js
+++ libs/middleHandleCli.js
@@ -1,6 +1,6 @@
 
 exports.init = function(argv, env) {
-    if (require('./filterCommand')(argv._[0]) && !env.configPath) {//如果没有任何规则，且没有fis-conf配置文件，则添加默认规则为`compress,parse`
+    if (require('./filterCommand')(argv._[0]) && !(argv.R || argv.rules) && !env.configPath) {//如果没有任何规则，且没有fis-conf配置文件，则添加默认规则为`compress,parse`
         argv.R = 'parse,compress';
     }
 }
